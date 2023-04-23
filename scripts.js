ba5e77a3b5c34297b79c6290c728cdcbc83b7ec3
salizarana <salizarana@gmail.com>
2023-04-23 17:31:59 +0545

    Add css and js file.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,16 @@
 let menuIcon = document.querySelector("#menu-icon");
+let videoBtn = document.querySelectorAll(".vid-btn");
 
 menuIcon.onclick = function() {
     navBar = document.querySelector(".nav-bar");
     navBar.classList.toggle("active");
 }
+
+videoBtn.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src;
+    });
+});
